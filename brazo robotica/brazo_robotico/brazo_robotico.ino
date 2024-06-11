040daa3d1ebab4fea768d3b629fbcf553036d161
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-06-11 13:50:48 +0200

    Final code

--- brazo robotica/brazo_robotico/brazo_robotico.ino
+++ brazo robotica/brazo_robotico/brazo_robotico.ino
@@ -23,7 +23,17 @@ void setUpServos(){
     servosY[i].attach(yOutput[i]);
   }
 }
+
+void checkAllOverValues(){
+  for(int i = 0; i<number; i++){
+    if(x[i] < 0){x[i] = 0;}
+    if(x[i] > 180){x[i] = 180;}
+    if(y[i] < 0){y[i] = 0;}
+    if(y[i] > 180){y[i] = 180;}
+  }
+}
 void sendAllServos(){
+  checkAllOverValues();
   for(int i = 0; i<number; i++){
     servosX[i].write(x[i]);
     servosY[i].write(y[i]);
@@ -42,22 +52,28 @@ void loop() {
     int xVal = map(xR, 0, 1023, 0, 179);
     int yVal = map(yR, 0, 1023, 0, 179);
     if(xVal < center - deadZone){
-      x[i] = x[i] - ;
+      x[i] = x[i] - sprintV;
     }
-    else if(xVal >= 80 && xVal < 88){
-      x[i] = x[i] - 3;
+    else if(xVal >= center - deadZone && xVal < center){
+      x[i] = x[i] - slowV;
     }
-    if(xVal > 96){
-      x[i] = x[i] + 10;
+    if(xVal > center + deadZone){
+      x[i] = x[i] + sprintV;
     }
-    else if(xVal <= 96 && xVal > 88){
-      x[i] = x[i] + 3;
+    else if(xVal <= center + deadZone && xVal > center){
+      x[i] = x[i] + slowV;
     }
-    if(yVal < 80){
-      y[i] = y[i] - 10;
+    if(yVal < center - deadZone){
+      y[i] = y[i] - sprintV;
     }
-    if(yVal > 96){
-      y[i] = y[i] + 10;
+    else if(yVal >= center - deadZone && yVal < center){
+      y[i] = y[i] - slowV;
+    }
+    if(yVal > center + deadZone){
+      y[i] = y[i] + sprintV;
+    }
+    else if(yVal <= center + deadZone && yVal > center){
+      y[i] = y[i] + slowV;
     }
   }
   delay(10);
@@ -66,14 +82,4 @@ void loop() {
 
   
  
-}
-
-
-void checkAllOverValues(){
-  for(int i = 0; i<number; i++){
-    if(x[i] < 0){x[i] = 0;}
-    if(x[i] > 180){x[i] = 180;}
-    if(y[i] < 0){y[i] = 0;}
-    if(y[i] > 180){y[i] = 180;}
-  }
 }
