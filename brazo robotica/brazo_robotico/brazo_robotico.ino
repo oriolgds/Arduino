4ff667516d9d780cb2f9621b297ac4ed0f5093f2
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-11-07 15:49:47 +0100

    Adding top file comments

--- brazo robotica/brazo_robotico/brazo_robotico.ino
+++ brazo robotica/brazo_robotico/brazo_robotico.ino
@@ -1,19 +1,41 @@
 #include <Servo.h>
+/*
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//      If any comment is removed the program will blow up 💣💣💣      //
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-int xInput[] = {A1};
-int yInput[] = {A0};
-int xOutput[] = {3};
-int yOutput[] = {5};
+Updates of this code are available at: 
+https://github.com/oriolgds/Arduino/blob/main/brazo%20robotica/brazo_robotico/brazo_robotico.ino
+
+
+*************************************************************************
+Code made by: Oriol Giner Díaz
+Cable connection made by: Oriol Giner Díaz
+
+If you want to see more Arduino projects about me head to:
+https://github.com/oriolgds/Arduino/
+
+To see more projects made by me that aren't Arduino head to:
+https://github.com/oriolgds/
+
+
+*/
+
+
+int xInput[] = {A1, A2};
+int yInput[] = {A0, A3};
+int xOutput[] = {3, 10};
+int yOutput[] = {5, 9};
 
 const int center = 88;
 const int deadZone = 50;
 const int centerDeadZone = 5;
 const int sprintV = 10;
 const int slowV = 1;
-int x[] = {center};
-int y[] = {center};
+int x[] = {center, center};
+int y[] = {center, center};
 
-const int number = 1;
+const int number = 2;
 
 Servo servosX[number];
 Servo servosY[number];
@@ -38,6 +60,7 @@ void sendAllServos(){
   for(int i = 0; i<number; i++){
     servosX[i].write(x[i]);
     servosY[i].write(y[i]);
+
   }
 }
 void setup() {
@@ -52,6 +75,8 @@ void loop() {
     int yR = analogRead(yInput[i]);
     int xVal = map(xR, 0, 1023, 0, 179);
     int yVal = map(yR, 0, 1023, 0, 179);
+    Serial.print("x Val: "); Serial.print(x[i]); Serial.print(", ");
+    //Serial.print("y Val: "); Serial.println(yVal);
     if(!(xVal > center - centerDeadZone && xVal < center + centerDeadZone)){
       if(xVal < center - deadZone){
         x[i] = x[i] - sprintV;
@@ -81,8 +106,8 @@ void loop() {
       }
     }    
   }
+  Serial.println(" ");
   sendAllServos();
-  delay(100);
-  
+  delay(100); 
  
 }
